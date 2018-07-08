e0cff2f5bfb7d9a3a3112fb17668eac963abd741
aneeshdurgbot <durg2+bot@illinois.edu>
2018-07-08 21:13:00 +0000

    Update at Sun Jul  8 21:13:00 UTC 2018

--- controller.py
+++ controller.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return open('/tmp/output', 'r').read()
+  return open('/tmp/output', 'r').read().replace('\n', '<br>')
 
 @app.route('/refresh')
 def refresh():
