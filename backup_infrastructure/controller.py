32fb2b9ad4a37456eb076a1a98ec478835e08acc
Aneesh Durg <durg2@illinois.edu>
2018-07-08 17:56:25 -0700

    Using templates

--- backup_infrastructure/controller.py
+++ backup_infrastructure/controller.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from os import system
 from subprocess import check_output
 
@@ -6,20 +6,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  index_template = open("templates/index.html", 'r').read()
   output = open('/tmp/output', 'r').read().replace('\n', '<br>')
-  return index_template.format(output)
+  return render_template('index.html', output=output)
 
 @app.route('/refresh')
 def refresh():
   exit_code = system('kill $(cat /tmp/sleep_pid)')
   status_str = 'STATUS: {}'.format(exit_code)
-  refresh_template = open("templates/refresh.html", 'r').read()
-  return refresh_template.format(status_str)
+  return render_template('refresh.html', status=status_str)
 
 @app.route('/set_interval')
 def set_interval():
-    set_interval_template = open("templates/set_interval.html", 'r').read()
     new_interval = request.args.get('interval', None)
     if new_interval is None:
       get_running_time = \
@@ -32,7 +29,10 @@ def set_interval():
 
       interval = int(open('/tmp/interval', 'r').read().strip())
 
-      return set_interval_template.format(interval, interval - running_time)
+      return render_template(
+          'set_interval.html',
+          interval=interval,
+          expiration=(interval - running_time))
 
     open('/tmp/interval', 'w').write(new_interval)
     return refresh()
